08c9b98d63a71afdbc15260179abfcae4cfc643d
Clark Kampfe <clark.kampfe@gmail.com>
2021-09-11 01:14:48 -0500

    add sierra lite, bayer, random threshold

--- benches/bench.rs
+++ benches/bench.rs
@@ -43,5 +43,66 @@ fn atkinson(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, floyd_steinberg, atkinson);
+fn sierra_lite(c: &mut Criterion) {
+    let img = ImageBuffer::from_fn(WIDTH, HEIGHT, |x, _y| {
+        if x % 2 == 0 {
+            image::Luma([0u8])
+        } else {
+            image::Luma([255u8])
+        }
+    });
+
+    c.bench_function("sierra lite", move |b| {
+        b.iter_batched(
+            || img.clone(),
+            |mut i| dither::dither_sierra_lite(&mut i, &image::imageops::BiLevel),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+fn bayer(c: &mut Criterion) {
+    let img = ImageBuffer::from_fn(WIDTH, HEIGHT, |x, _y| {
+        if x % 2 == 0 {
+            image::Luma([0u8])
+        } else {
+            image::Luma([255u8])
+        }
+    });
+
+    c.bench_function("bayer", move |b| {
+        b.iter_batched(
+            || img.clone(),
+            |mut i| dither::dither_bayer(&mut i, &image::imageops::BiLevel),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+fn random_threshold(c: &mut Criterion) {
+    let img = ImageBuffer::from_fn(WIDTH, HEIGHT, |x, _y| {
+        if x % 2 == 0 {
+            image::Luma([0u8])
+        } else {
+            image::Luma([255u8])
+        }
+    });
+
+    c.bench_function("random threshold", move |b| {
+        b.iter_batched(
+            || img.clone(),
+            |mut i| dither::dither_random_threshold(&mut i, &image::imageops::BiLevel),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+criterion_group!(
+    benches,
+    floyd_steinberg,
+    atkinson,
+    sierra_lite,
+    bayer,
+    random_threshold
+);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,6 @@ where
     image::imageops::dither(image, color_map)
 }
 
-// based on this Python implementation, found here: http://mike.teczno.com/notes/atkinson.html
-//
 // import sys, PIL.Image
 //
 // img = PIL.Image.open(sys.argv[-1]).convert('L')
@@ -32,6 +30,7 @@ where
 //                 pass
 //
 // img.show()
+/// based on this Python implementation, found here: http://mike.teczno.com/notes/atkinson.html
 pub fn dither_atkinson<Pix, Map>(image: &mut ImageBuffer<Pix, Vec<u8>>, color_map: &Map)
 where
     Map: ColorMap<Color = Pix> + ?Sized,
@@ -80,6 +79,104 @@ where
                     }
                 }
             }
+        }
+    }
+}
+
+///   * 2   The Sierra-2-4A filter
+/// 1 1     (1/4)
+pub fn dither_sierra_lite<Pix, Map>(image: &mut ImageBuffer<Pix, Vec<u8>>, color_map: &Map)
+where
+    Map: ColorMap<Color = Pix> + ?Sized,
+    Pix: Pixel<Subpixel = u8> + 'static,
+{
+    let width = image.width();
+    let height = image.height();
+
+    let mut mask: [([u32; 2], u8); 3];
+
+    let mut error: [i16; 3];
+
+    for y in 1..(height - 1) {
+        for x in 1..(width - 1) {
+            let old_pixel = image[(x, y)];
+            let new_pixel = image.get_pixel_mut(x, y);
+            color_map.map_color(new_pixel);
+
+            error = [0i16; 3];
+
+            for ((e, &old), &new) in error
+                .iter_mut()
+                .zip(old_pixel.channels().iter())
+                .zip(new_pixel.channels().iter())
+            {
+                *e = (i16::from(old) - i16::from(new)) / 4
+            }
+
+            mask = [([x + 1, y], 2), ([x - 1, y - 1], 1), ([x, y + 1], 1)];
+
+            for ([x, y], factor) in mask {
+                let pixel = image.get_pixel_mut(x, y);
+
+                for (e, c) in error.iter().zip(pixel.channels_mut().iter_mut()) {
+                    *c = match i16::from(*c) + e * factor as i16 {
+                        val if val < 0 => 0u8,
+                        val if val > 0xFF => 0xFFu8,
+                        val => val as u8,
+                    }
+                }
+            }
+        }
+    }
+}
+
+/// https://web.archive.org/web/20190316064436/http://www.efg2.com/Lab/Library/ImageProcessing/DHALF.TXT
+pub fn dither_bayer<Pix, Map>(image: &mut ImageBuffer<Pix, Vec<u8>>, _color_map: &Map)
+where
+    Map: ColorMap<Color = Pix> + ?Sized,
+    Pix: Pixel<Subpixel = u8> + 'static,
+{
+    let pattern: [[u8; 8]; 8] = [
+        [0, 32, 8, 40, 2, 34, 10, 42],    /* 8x8 Bayer ordered dithering  */
+        [48, 16, 56, 24, 50, 18, 58, 26], /* pattern.  Each input pixel   */
+        [12, 44, 4, 36, 14, 46, 6, 38],   /* is scaled to the 0..63 range */
+        [60, 28, 52, 20, 62, 30, 54, 22], /* before looking in this table */
+        [3, 35, 11, 43, 1, 33, 9, 41],    /* to determine the action.     */
+        [51, 19, 59, 27, 49, 17, 57, 25],
+        [15, 47, 7, 39, 13, 45, 5, 37],
+        [63, 31, 55, 23, 61, 29, 53, 21],
+    ];
+
+    for (x, y, pixel) in image.enumerate_pixels_mut() {
+        for channel in pixel.channels_mut() {
+            let scaled = *channel >> 2;
+            assert!(scaled < 64);
+            if scaled > pattern[x as usize & 7][y as usize & 7] {
+                *channel = 255;
+            }
+        }
+    }
+}
+
+//     grayscaleImage.mapSelf(brightness =>
+//   brightness + (Math.random() - 0.5) > 0.5
+//     ? 1.0
+//     : 0.0
+// );
+/// https://surma.dev/things/ditherpunk/
+pub fn dither_random_threshold<Pix, Map>(image: &mut ImageBuffer<Pix, Vec<u8>>, _color_map: &Map)
+where
+    Map: ColorMap<Color = Pix> + ?Sized,
+    Pix: Pixel<Subpixel = u8> + 'static,
+{
+    for pixel in image.pixels_mut() {
+        let r: i8 = rand::random();
+        for channel in pixel.channels_mut() {
+            *channel = if i16::from(*channel) + i16::from(r - 127i8) > 127 {
+                255
+            } else {
+                0
+            };
         }
     }
 }
